af5038b74903ebc0950eec66e51a2e9b6d0b0e27
t.morovati <t.morovati.99@gmail.com>
2019-12-12 14:37:18 +0330

    this is first srep in learning oop programming

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std ; 
+
+int main ()
+{ 
+cout << "hey there "; 
+cout << "this is my first time in git " ;  
+cout << "21th azar 98";  
+return 0 ;  
+ 
+}
+
+
+
+
+
